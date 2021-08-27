541f58d7707adee0aa5e6322841985cbc19cd4ae
Jesse <xinthral@yahoo.com>
2021-08-27 18:11:59 -0400

    Adding Testing templates

---
+++ Test.cpp
@@ -0,0 +1,9 @@
+/**************************************
+# Author: Jesse Hassell
+# Date: Aug, 2021
+# Purpose: This file is meant to serve as a testing
+# bed for new functionality and ensure stability
+# through high entropy.
+**************************************/
+
+#include "Test.h"
---
+++ Test.h
@@ -0,0 +1,7 @@
+#ifndef TEST_H
+#define TEST_H
+
+class Test {
+    // code here
+};
+#endif
